0277a906db71cce6d510bd68f5d61b417ef9cc83
Guilherme-spec2 <66727685+Guilherme-spec2@users.noreply.github.com>
2020-07-10 01:09:08 -0300

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,13 +1,16 @@
 import requests
+import pyfiglet
 from time import sleep
+
+ascii_banner = pyfiglet.figlet_format("Programmed By Slaki")
+print(ascii_banner)
 print(""""
 ############################
 #  Brute Admin/Diretorio   #
 ############################
 """)
-
 def paineladmin():
-    site = input("Digite a url: ")
+    site = input("Digite a url, exemplo:https://www.exemplo.com.br: ").strip()
     word = open("wordlist.txt")
     proxy = {
     'https':'200.255.122.170:8080'
@@ -16,14 +19,15 @@ def paineladmin():
         'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
     }
     for wor in word:
-         sit = site + wor.strip()
+         sit = site + '/' + wor.strip()
          bru = requests.get(sit,proxies = proxy,headers = header)
          sleep(3)
          if bru.status_code == 200:
              print(f'Painel encontrado {sit} Status: {bru.status_code}')
-
+         else:
+            print(sit,bru.status_code)
 def diretorio():
-    site = input("Digite a url: ")
+    site = input("Digite a url, exemplo:https://www.exemplo.com.br: ").strip()
     proxy = {
         'https': '200.255.122.170:8080'
     }
@@ -32,29 +36,28 @@ def diretorio():
     }
     dir = open('directy.txt')
     for wor1 in dir:
-        sit = site + wor1.strip()
-        bru = requests.get(sit, proxies=proxy, headers=header)
+        sit = site + '/' + wor1.strip()
+        bru = requests.get(sit,proxies = proxy,headers=header)
+        sleep(3)
         if bru.status_code == 200:
             print(f'Diretorio encontrado {sit} {bru.status_code}')
         else:
             print(sit,bru.status_code)
-try:
-    opcao = int(input("Escolha uma opção\n 1- Painel admin \n 2-Brute Diretorio \n "))
-    if opcao == 1:
-        print(""""
+
+opcao = int(input("Escolha uma opção\n 1 - Painel Admin \n 2 - Brute Diretorio \n "))
+if opcao == 1:
+    print(""""
                 ############################
                 #       Painel admin       #
                 ############################
                 """)
-        paineladmin()
-    elif opcao == 2:
-        print(""""
+    paineladmin()
+elif opcao == 2:
+    print(""""
         ############################
         #       Brute Diretorio    #
         ############################
         """)
-        diretorio()
-    elif opcao < 1  or opcao > 2:
-        print("opção invalida, tente novamente.")
-except:
-    print("Opção invalida, tente novamente.")
+    diretorio()
+elif opcao < 1  or opcao > 2:
+    print("opção invalida, tente novamente.")
